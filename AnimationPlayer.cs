9cb4cd5cf1a565e3a493dd83e75daa5fe6b9b388
DreaDream <51436796+DreaDream@users.noreply.github.com>
2021-01-15 15:44:52 +0100

    Title
    Description

---
+++ AnimationPlayer.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class AnimationPlayer : Godot.AnimationPlayer
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		((AnimationPlayer)GetNode("AnimationPlayer")).Play("RunRight");
+	}
+
+public void start()
+{
+	anim = gameObject.GetComponent<gof>();
+	
+}
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
