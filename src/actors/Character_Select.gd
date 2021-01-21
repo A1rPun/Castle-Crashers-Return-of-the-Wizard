801bc06dd6f8bd1d7cdbbd8fe5610363c7aaa777
A1rPun <thombakker@gmail.com>
2021-01-21 02:53:54 +0100

    :sparkles: Add character to select screen

---
+++ src/actors/Character_Select.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		get_tree().change_scene("res://src/levels/NecroLair.tscn");
+	elif Input.is_action_just_pressed("use_item"):
+		get_tree().change_scene("res://src/game/Main_Menu.tscn");
