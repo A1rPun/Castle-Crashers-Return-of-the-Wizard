c237048b2afcbf5e8fc452525c61e236dfdd9e1f
A1rPun <thombakker@gmail.com>
2021-02-05 17:37:56 +0100

    :bug: Small changes

--- src/actors/Main_Menu.gd
+++ src/actors/Main_Menu.gd
@@ -4,6 +4,12 @@ func _ready():
 	for button in $Buttons.get_children():
 		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
 
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		get_tree().change_scene("res://src/game/Character_Select.tscn");
+	elif Input.is_action_just_pressed("use_item"):
+		_on_QuitButton_pressed();
+
 func _on_button_pressed(scene_to_load):
 	get_tree().change_scene(scene_to_load)
 
